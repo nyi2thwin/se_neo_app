e9913e1a021193517da548511c7aef75d71a2299
thichbanla <thichbanla@gmail.com>
2018-03-24 11:45:03 +0800

    edit bookingModel

--- models/Booking.js
+++ models/Booking.js
@@ -1,6 +1,20 @@
-class Booking {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
-  }
-}
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookingSchema = new Schema({
+  clinicId: Number,
+  dateTime: Date,
+  userId: Number,
+  queNo: Number,
+  status: String,
+  estimatedTime: Date
+});
+
+BookingSchema.save(function(err){
+  if(err) throw err;
+
+  console.log("Booking is saved.");
+});
+
+module.exports = mongoose.model('User', UserSchema);
