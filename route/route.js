27a9c536c4f81abd902c3db6c9bc08147e53e0dc
htethtet <htethtetchit1193@gmail.com>
2018-03-25 22:06:05 +0800

    Update route.js

--- route/route.js
+++ route/route.js
@@ -2,10 +2,12 @@
 module.exports = function(app){
 	var loginController = require('../controllers/loginController');
 	
-	app.route('/login/:email')
-	.get(loginController.readUserByEmail);
+	app.route('/getAllusers')
+	.get(loginController.list_all_users);
 	
-	app.route('/users')
-	.get(loginController.list_all_users)
+	app.route('/register')
 	.post(loginController.registerNewUser);
+	
+	app.route('/findUserById')
+	.post(loginController.findUserById);
 };
