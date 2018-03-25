5f662036381ebb662fc5823c7794d6dfe5448522
htethtet <htethtetchit1193@gmail.com>
2018-03-25 22:02:53 +0800

    Update login.controller.js

--- public/angular_frontend/login/login.controller.js
+++ public/angular_frontend/login/login.controller.js
@@ -1,24 +1,42 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular
         .module('app')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, AuthenticationService, FlashService) {
+    LoginController.$inject = ['$location', '$http', 'FlashService'];
+    function LoginController($location, $http, FlashService) {
         var vm = this;
-
+		var loginURL = "http://localhost:3000/findUserById";
         vm.login = login;
 
-        (function initController() {
+       /* (function initController() {
             // reset login status
             AuthenticationService.ClearCredentials();
         })();
-
+		*/
         function login() {
             vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
+			var userId = {"userId":vm.username};
+			$http.post(loginURL, userId).then(
+            function(response){
+                if (response.statusText == "OK") {
+						if(response.data.password == vm.password){
+							$location.path('/');
+						}
+						else{
+							FlashService.Error("Invalid Password");
+							vm.dataLoading = false;
+						}
+                                          
+				} else {
+					FlashService.Error("Invalid NRIC");
+					vm.dataLoading = false;
+				}
+            });
+			
+           /* AuthenticationService.Login(vm.username, vm.password, function (response) {
                 if (response.success) {
                     AuthenticationService.SetCredentials(vm.username, vm.password);
                     $location.path('/');
@@ -26,7 +44,7 @@
                     FlashService.Error(response.message);
                     vm.dataLoading = false;
                 }
-            });
+            }); */
         };
     }
 
