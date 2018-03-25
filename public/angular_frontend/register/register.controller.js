5a04aedb605f789b227b45846fd2216adccd2122
htethtet <htethtetchit1193@gmail.com>
2018-03-25 22:04:12 +0800

    Update register.controller.js

--- public/angular_frontend/register/register.controller.js
+++ public/angular_frontend/register/register.controller.js
@@ -1,29 +1,52 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular
         .module('app')
         .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
+    RegisterController.$inject = ['$location', '$rootScope','$http','$scope','FlashService'];
+    function RegisterController($location, $rootScope,$http,$scope,FlashService) {
         var vm = this;
+        var registerURL = "http://localhost:3000/register";
 
         vm.register = register;
 
         function register() {
             vm.dataLoading = true;
-            UserService.Create(vm.user)
-                .then(function (response) {
-                    if (response.success) {
+            var dataToSend = 
+			{
+				"userId":vm.user.nric,
+				"email":vm.user.email,
+				"name":vm.user.lastName,
+				"contact":vm.user.contact,
+				"dob":vm.user.dob,
+				"password":vm.user.password
+			};             
+            $http.post(registerURL, dataToSend).then(
+            function(response){
+                if (response.statusText == "OK") {
                         FlashService.Success('Registration successful', true);
                         $location.path('/login');
                     } else {
-                        FlashService.Error(response.message);
+                        FlashService.Error(response.statusText);
                         vm.dataLoading = false;
                     }
-                });
+            });
         }
-    }
+		
+		/*$scope.dobChanged = function () {
+
+				 $("#dob").datepicker({
+					   dateFormat: 'dd/mm/yy',
+					   onClose: function (text, datePicker) {
+							  if (!!text) {
+									 vm.user.dob = new Date(datePicker.selectedYear, datePicker.selectedMonth, datePicker.selectedDay);
+									  $(this).datepicker('setDate', new Date(datePicker.selectedYear, datePicker.selectedMonth, datePicker.selectedDay));
+							  };
+					   }
+				 });
+		}*/
+	}
 
 })();
