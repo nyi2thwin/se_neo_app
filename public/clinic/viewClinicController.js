61be83837689c971eb167641dc6460e40d617927
htethtet <htethtetchit1193@gmail.com>
2018-04-02 20:28:17 +0800

    Add files via upload

---
+++ public/clinic/viewClinicController.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('viewClinicController', viewClinicController);
+
+    viewClinicController.$inject = ['$scope', '$http', '$filter', '$location','$rootScope'];
+    function viewClinicController($scope, $http, $filter, $location,$rootScope) {
+		 var vm = this;
+		 var findClinicByClinicIdURL = "http://localhost:3000/findClinicById";
+		 var updateMyClinicInfoURL = "http://localhost:3000/updateClinic"; //To be update the link
+		 var clinicId = "5abedbf6f2dc26bda0fb7df4"; //hard-code for testing purpose
+		 $scope.mdata = {};
+		 var dataBeforeUpdate = {};
+		 $scope.disableForm = true;
+		 vm.dataLoading = false;
+		 var init = function(){
+            
+            var dataToSend = 
+			{
+				"clinicId":clinicId,
+			};             
+            $http.post(findClinicByClinicIdURL, dataToSend).then(
+            function(response){
+                if (response.statusText == "OK") {
+                    $scope.mdata = response.data;
+					dataBeforeUpdate = angular.copy($scope.mdata);
+				} else {
+					FlashService.Error(response.statusText);
+					vm.dataLoading = false;
+				}
+            },
+			function (response) {                          
+				FlashService.Error(response.statusText);
+				vm.dataLoading = false;
+			});
+		}
+		
+		$scope.enableFormEdit = function(){
+			$scope.disableForm = false;
+		}
+		$scope.cancelFormEditing = function(){
+			$scope.disableForm = true;
+			$scope.mdata = angular.copy(dataBeforeUpdate);
+		}
+		$scope.updateMyClinicInfo = function(){
+			var dataToSend = $scope.mdata;
+			vm.dataLoading = true;
+			$http.post(updateMyClinicInfoURL, dataToSend).then(
+            function(response){
+                if (response.statusText == "OK") {
+                    $scope.mdata = response.data;
+					$scope.cancelFormEditing();
+					lashService.Success('Update successful', true);
+				} else {
+					FlashService.Error(response.statusText);
+					vm.dataLoading = false;
+				}
+            },
+			function (response) {                          
+				FlashService.Error(response.statusText);
+				vm.dataLoading = false;
+			});
+		}
+		init();
+		
+    }
+
+})();
