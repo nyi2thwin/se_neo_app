479aa2fc03a2667920001f336e8e32a742d96a2b
htethtet <htethtetchit1193@gmail.com>
2018-04-06 16:14:33 +0800

    upgrade Map and added markers

--- public/home/homeController.js
+++ public/home/homeController.js
@@ -8,8 +8,42 @@
     homeController.$inject = ['$scope', '$http', '$filter', '$location','$rootScope','FlashService'];
     function homeController($scope, $http, $filter, $location,$rootScope,FlashService) {
 		var vm = this;
+		//Test Data
+		var cities = [
+              {
+                  city : 'India',
+                  desc : 'This is the best country in the world!',
+                  lat : 23.200000,
+                  long : 79.225487
+              },
+              {
+                  city : 'New Delhi',
+                  desc : 'The Heart of India!',
+                  lat : 28.500000,
+                  long : 77.250000
+              },
+              {
+                  city : 'Mumbai',
+                  desc : 'Bollywood city!',
+                  lat : 19.000000,
+                  long : 72.90000
+              },
+              {
+                  city : 'Kolkata',
+                  desc : 'Howrah Bridge!',
+                  lat : 22.500000,
+                  long : 88.400000
+              },
+              {
+                  city : 'Chennai  ',
+                  desc : 'Kathipara Bridge!',
+                  lat : 13.000000,
+                  long : 80.250000
+              }
+          ];
 		$scope.hideSearchBox = true;
-		$scope.hideResult = false;
+		$scope.hideSearchResult = true;
+		$scope.hideDetailResult = true;
 		var loggedIn = $rootScope.globals.currentUser;
 		if(!loggedIn){
 			$rootScope.isGuest = true;
@@ -32,6 +66,11 @@
                     $scope.clinicList = response.data;
 					vm.dataLoading = false;
 					$scope.hideSearchBox = false;
+					$scope.hideSearchResult = false;
+					$scope.hideDetailResult = true;
+					for (var i = 0; i < $scope.clinicList.length; i++){
+						createMarker($scope.clinicList[i]);
+					}
 				} else {
 					FlashService.Error(response.statusText);
 					vm.dataLoading = false;
@@ -40,11 +79,13 @@
 		
         }
 		vm.viewDetail = function(clinic){
-			$scope.hideResult = true;
+			$scope.hideSearchResult = true;
+			$scope.hideDetailResult = false;
 			$scope.mdata.clinic = clinic;
 		}
 		vm.backToResult = function(){
-			$scope.hideResult = false;
+			$scope.hideSearchResult = false;
+			$scope.hideDetailResult = true;
 		}
 		
 		vm.makeAppoint = function(){
@@ -97,6 +138,41 @@
             });
 		}
 		
+		var mapOptions = {
+                  zoom: 5,
+                  center: new google.maps.LatLng(1.290270,103.851959),
+                  mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+
+		$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+		$scope.markers = [];
+	  
+		var infoWindow = new google.maps.InfoWindow();
+		
+		var createMarker = function (clinic){
+                  
+			var marker = new google.maps.Marker({
+				map: $scope.map,
+				position: new google.maps.LatLng(clinic.location[0], clinic.location[1]),
+				title:clinic.name
+			});
+			marker.content = '<div class="infoWindowContent">' + clinic.address + '</div>';
+		  
+			google.maps.event.addListener(marker, 'click', function(){
+				infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+				infoWindow.open($scope.map, marker);
+			});
+		  
+			$scope.markers.push(marker);
+                  
+        }  
+		
+		$scope.openInfoWindow = function(e, selectedMarker){
+			e.preventDefault();
+			google.maps.event.trigger(selectedMarker, 'click');
+		}
+
     }
 
 })();
