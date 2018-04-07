8426d20c425e021623193bab51c85f4776be0442
htethtet <htethtetchit1193@gmail.com>
2018-04-07 12:21:34 +0800

    rating stars and disable function for appointment

--- public/home/homeController.js
+++ public/home/homeController.js
@@ -8,39 +8,7 @@
     homeController.$inject = ['$scope', '$http', '$filter', '$location','$rootScope','FlashService'];
     function homeController($scope, $http, $filter, $location,$rootScope,FlashService) {
 		var vm = this;
-		//Test Data
-		var cities = [
-              {
-                  city : 'India',
-                  desc : 'This is the best country in the world!',
-                  lat : 23.200000,
-                  long : 79.225487
-              },
-              {
-                  city : 'New Delhi',
-                  desc : 'The Heart of India!',
-                  lat : 28.500000,
-                  long : 77.250000
-              },
-              {
-                  city : 'Mumbai',
-                  desc : 'Bollywood city!',
-                  lat : 19.000000,
-                  long : 72.90000
-              },
-              {
-                  city : 'Kolkata',
-                  desc : 'Howrah Bridge!',
-                  lat : 22.500000,
-                  long : 88.400000
-              },
-              {
-                  city : 'Chennai  ',
-                  desc : 'Kathipara Bridge!',
-                  lat : 13.000000,
-                  long : 80.250000
-              }
-          ];
+		
 		$scope.hideSearchBox = true;
 		$scope.hideSearchResult = true;
 		$scope.hideDetailResult = true;
