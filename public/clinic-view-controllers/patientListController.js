5c7ad79d4b66a85eaccb76ef915564f99fb95dff
htethtet <htethtetchit1193@gmail.com>
2018-04-10 14:04:18 +0800

    refactoring the folders to view,controller,model

---
+++ public/clinic-view-controllers/patientListController.js
@@ -0,0 +1,88 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('patientListController', patientListController);
+
+    patientListController.$inject = ['$scope', '$http', '$filter', '$location','$rootScope','FlashService'];
+    function patientListController($scope, $http, $filter, $location,$rootScope,FlashService) {
+		var vm = this;
+		var findBookingByClinicIdURL = "/findBookingByClinicId";
+		var clinicId = $rootScope.globals.currentUser.id;
+		$scope.patientList = {};
+
+		$scope.unqueue = function(bookingId){
+			vm.dataLoading = true;
+			var dataToSend = 
+			{
+				"bookingId":bookingId,
+			};             
+			$http.post("/deleteBooking", dataToSend).then(
+				function(response){
+				    if (response.statusText == "OK") {
+				    	console.log(response);
+						FlashService.Success(response.data.message);
+						init();
+					} else {
+						FlashService.Error(response.statusText);
+						vm.dataLoading = false;
+					}
+				},	
+				function (response) {
+					vm.dataLoading = false;
+					FlashService.Error("Error Connection Refused");
+				});
+
+		}
+		$scope.notify = function(bookingId){
+			vm.dataLoading = true;
+			var dataToSend = 
+			{
+				"bookingId":bookingId,
+			};             
+			$http.post("/sendNotification", dataToSend).then(
+				function(response){
+				    if (response.statusText == "OK") {
+				    	console.log(response);
+						FlashService.Success(response.data.message);
+						init();
+					} else {
+						FlashService.Error(response.statusText);
+						vm.dataLoading = false;
+					}
+				},
+				function (response) {
+					vm.dataLoading = false;
+					FlashService.Error("Error Connection Refused");
+				});
+
+		}
+		var init = function(){
+			vm.dataLoading = true;
+			var dataToSend = 
+			{
+				"clinicId":clinicId,
+			};             
+			$http.post(findBookingByClinicIdURL, dataToSend).then(
+				function(response){
+				    if (response.statusText == "OK") {
+						if(response.data.length == 0){
+							FlashService.Error("No Patients in Queue!");
+						}
+				        $scope.patientList = response.data;
+					} else {
+						FlashService.Error(response.statusText);
+						vm.dataLoading = false;
+					}
+				},
+				function (response) {
+					vm.dataLoading = false;
+					FlashService.Error("Error Connection Refused");
+				});
+
+			}
+		init();
+    }
+
+})();
