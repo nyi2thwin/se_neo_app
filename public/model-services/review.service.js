15f1f0b8b91c7aa5a3b8f8a09bfd243313987cdc
htethtet <htethtetchit1193@gmail.com>
2018-04-11 14:22:47 +0800

    add Review.service

---
+++ public/model-services/review.service.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('Review', Review);
+
+    Review.$inject = ['$rootScope','$http'];
+    function Review($rootScope,$http) {
+        var service = {};
+		service.AddReview = AddReview;
+        return service;
+		
+		function AddReview(dataToSend) {
+			return $http.post("http://localhost:3000/addReview", dataToSend).then(handleSuccess, handleError('Error finding clinic by Id'));
+		}
+
+		//Private Methods
+	    function handleSuccess(res) {
+            return { success: true, data: res.data };
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+
+    }
+
+})();
